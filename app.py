c038e9d93f3f31af44b4156022c0a38c9de96554
millivan <limzyu02@gmail.com>
2024-04-24 20:47:42 +0800

    haizz

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 # @app.route("/")
 # def greeting():
 #     return "Flask is Awesome!"
-model = pickle.load(open("example_weights_knn.pkl", "rb"))
+# model = pickle.load(open("example_weights_knn.pkl", "rb"))
 
 
 @app.route("/")
@@ -19,45 +19,45 @@ def use_template():
     return "Hi"
 
 
-@app.route("/predict", methods=["POST", "GET"])
-def predict():
-    input_one = request.form["1"]
-    input_two = request.form["2"]
-    input_three = request.form["3"]
-    input_four = request.form["4"]
-    input_five = request.form["5"]
-    input_six = request.form["6"]
-    input_seven = request.form["7"]
-    input_eight = request.form["8"]
+# @app.route("/predict", methods=["POST", "GET"])
+# def predict():
+#     input_one = request.form["1"]
+#     input_two = request.form["2"]
+#     input_three = request.form["3"]
+#     input_four = request.form["4"]
+#     input_five = request.form["5"]
+#     input_six = request.form["6"]
+#     input_seven = request.form["7"]
+#     input_eight = request.form["8"]
 
-    setup_df = pd.DataFrame(
-        [
-            pd.Series(
-                [
-                    input_one,
-                    input_two,
-                    input_three,
-                    input_four,
-                    input_five,
-                    input_six,
-                    input_seven,
-                    input_eight,
-                ]
-            )
-        ]
-    )
-    diabetes_prediction = model.predict_proba(setup_df)
-    output = "{0:.{1}f}".format(diabetes_prediction[0][1], 2)
-    output = float(output)
+#     setup_df = pd.DataFrame(
+#         [
+#             pd.Series(
+#                 [
+#                     input_one,
+#                     input_two,
+#                     input_three,
+#                     input_four,
+#                     input_five,
+#                     input_six,
+#                     input_seven,
+#                     input_eight,
+#                 ]
+#             )
+#         ]
+#     )
+#     diabetes_prediction = model.predict_proba(setup_df)
+#     output = "{0:.{1}f}".format(diabetes_prediction[0][1], 2)
+#     output = float(output)
 
-    if output > 0.5:
-        return render_template(
-            "result.html", pred="You may have diabetes based on our KNN model "
-        )
-    else:
-        return render_template(
-            "result.html", pred="Prediction shows you don't have diabetes"
-        )
+#     if output > 0.5:
+#         return render_template(
+#             "result.html", pred="You may have diabetes based on our KNN model "
+#         )
+#     else:
+#         return render_template(
+#             "result.html", pred="Prediction shows you don't have diabetes"
+#         )
 
 
 # if __name__ == "__main__":
